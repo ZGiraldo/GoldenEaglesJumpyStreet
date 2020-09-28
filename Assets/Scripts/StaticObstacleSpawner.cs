e61de5da36a9bf301aae8492f28a6901aecfe0a5
LAPTOP-KJBM1VVT\Marcel <valdezmarcel@ymail.com>
2020-09-28 15:16:25 -0400

    Added New Scene for Generator Testing Added Static Object Generator Script Added Static Object Prefabs

---
+++ Assets/Scripts/StaticObstacleSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> obstacleVariations = new List<GameObject>();
+    [SerializeField] private List<bool> ocuppiedPos = new List<bool>();
+
+    private int randomAmount = 0;
+
+
+    void Start()
+    {
+        randomAmount = Random.Range(7, 11); //Amount of obstacles per Terrain
+
+        for(int i = 0; i < randomAmount; i++)
+        {
+            int randomXPos = Random.Range(0, 20); //the max for randomXPos should be the X scale of the terrains
+
+            if (!ocuppiedPos[randomXPos])
+            {
+                ocuppiedPos[randomXPos] = true;
+                Instantiate(obstacleVariations[Random.Range(0, obstacleVariations.Count)], new Vector3(randomXPos - 9.5f, 1, transform.position.z), Quaternion.Euler(-90,0,0), transform); // subtract (randomXPos/2 - .5) from randomXPos
+            }
+            else
+            {
+                i--;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
