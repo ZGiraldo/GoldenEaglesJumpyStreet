54e33395ade44d085b24178de1d351c5e0962611
kcavales <kcavales@my.waketech.edu>
2020-09-30 14:15:56 -0400

    Update: player rotation, player anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject playerModel;
+    public Animator anim;
+
     [SerializeField] float moveSpeed = 0;
     [SerializeField] float length = 0;
+    [SerializeField] float rotationSpeed = 0;
 
     Vector3 targetPosition;
     Vector3 startPosition;
     bool isMoving;
-    
 
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = GetComponentInChildren<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -32,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 transform.position = targetPosition;
                 isMoving = false;
+                anim.SetBool("isJumping", false);
                 return;
             }
             transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
@@ -40,35 +43,48 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             MovementDirection(Vector3.forward);
+            RotatePlayer(-90, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             MovementDirection(Vector3.back);
+            RotatePlayer(-90, 180, 0);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
             MovementDirection(Vector3.left);
+            RotatePlayer(-90, -90, 0);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             MovementDirection(Vector3.right);
+            RotatePlayer(-90, 90, 0);
         }
 
-        /*
-      float x = Input.GetAxisRaw("Horizontal");
-      float z = Input.GetAxisRaw("Vertical");
+        Debug.DrawRay(transform.position, Vector3.forward * 0.5f, Color.red);
+        Debug.DrawRay(transform.position, Vector3.back * 0.5f, Color.red);
+        Debug.DrawRay(transform.position, Vector3.left * 0.5f, Color.red);
+        Debug.DrawRay(transform.position, Vector3.right * 0.5f, Color.red);
 
-      if(x == 1f)
-      */
     }
 
     void MovementDirection(Vector3 direction)
     {
+
         if (!Physics.Raycast(transform.position, direction, length))
         {
             targetPosition = transform.position + direction;
             startPosition = transform.position;
             isMoving = true;
+
+            anim.SetBool("isJumping", true);
         }
+
+    }
+
+    void RotatePlayer(float x, float y, float z)
+    {
+        Quaternion target = Quaternion.Euler(x, y, z);
+        playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, target, rotationSpeed);
     }
 }
