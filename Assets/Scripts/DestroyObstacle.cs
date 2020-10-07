105b64683cef3974fca82db8a1ff062bd893060a
ZGiraldo <zwgiraldo@my.waketech.edu>
2020-10-07 14:24:47 -0400

    River prefab changes

--- Assets/Scripts/DestroyObstacle.cs
+++ Assets/Scripts/DestroyObstacle.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class DestroyObstacle : MonoBehaviour
 {
-
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
     public void OnCollisionEnter(Collision collision)
     {
         Destroy(collision.gameObject);
